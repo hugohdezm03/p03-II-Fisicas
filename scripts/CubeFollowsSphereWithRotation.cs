0f5492f32338c17d633503b80f1da8e9728e41fc
Hugo Hernández Martín <91559345+hugohdezm03@users.noreply.github.com>
2024-10-13 19:37:26 +0100

    Adición Script act 77

---
+++ scripts/CubeFollowsSphereWithRotation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFollowsSphereWithRotation : MonoBehaviour
+{
+    public float sphereSpeed = 5.0f;
+    public float cubeSpeed = 3.0f;
+    private GameObject cube;
+    private GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.Find("Cube");
+        sphere = GameObject.Find("Sphere");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 sphere_direction = new Vector3(Input.GetAxis("New Letters Horizontal"), Input.GetAxis("New Letters Vertical"), 0.0f);
+        sphere.transform.Translate(sphere_direction * sphereSpeed * Time.deltaTime, Space.Self);
+        Vector3 cube_direction = Vector3.Normalize(sphere.transform.position - cube.transform.position);
+        cube.transform.LookAt(sphere.transform);
+        cube.transform.Translate(cube_direction * cubeSpeed * Time.deltaTime, Space.World);
+        // cube.transform.Translate(cube_direction * cubeSpeed * Time.deltaTime, Space.Self);
+    }
+}
